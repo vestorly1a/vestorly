0c966c1f90892ba3667177168a51178d68ac2e3a
Avery Lamp <averylamp@gmail.com>
2015-08-08 15:23:53 -0400

    more edits

--- iOS/SwiftPages/SwiftPages/SearchViewController.swift
+++ iOS/SwiftPages/SwiftPages/SearchViewController.swift
@@ -60,13 +60,40 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
                 if json != nil {
                     self.articlesToDisplay = JSON(json!)
                     self.tableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Left)
-                    
+//                    self.loadAllImages(self.articlesToDisplay)
                 }
             }
         })
         
         
     }
+    var imageArray: Array<UIImageView>! = nil
+    
+//    func loadAllImages(json:JSON){
+//        for i in 0...articlesToDisplay["articles"].count{
+//            let url = articlesToDisplay["articles"][i]["image_url"].string
+//            print(url)
+//            
+//            if (url != nil){
+//                let image_url = NSURL(string:"https:\(url!)")
+//                
+//                let priority = DISPATCH_QUEUE_PRIORITY_BACKGROUND
+//                dispatch_async(dispatch_get_global_queue(priority, 0)) {
+//                    // do some task
+//                    let image_data = NSData(contentsOfURL: image_url!)
+//                    
+//                    dispatch_async(dispatch_get_main_queue()) {
+//                        // update some UI
+//                        let image = UIImage(data: image_data!)
+//                        let imageView = UIImageView(image: image)
+//                        imageView.tag = i
+//                        self.imageArray.append(imageView)
+//                        
+//                    }
+//                }
+//            }
+//        }
+//    }
     
     func loadAllArticles(){
         let URL = NSURL(string: "https://staging.vestorly.com/api/v2/articles?vestorly_auth=eyJwYXlsb2FkIjoiNTVjNTU0YTA3ZDY4NzhhN2FjMDAwMDAxIiwiY3JlYXRlZF9vbiI6MTQzOTA0NTc5NCwic2lnbmF0dXJlIjoiZGRtaE1ZNFo4c0dFSmdHaVhvZ1lzcVc4VTdGZ1JkaHNkNiswS2ZFYThWST0ifQ&limit=300")
@@ -83,7 +110,7 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
                 if json != nil {
                     self.articlesToDisplay = JSON(json!)
                     self.tableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Left)
-                    
+//                    self.loadAllImages(self.articlesToDisplay)
                 }
             }
         })
@@ -103,9 +130,13 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
     }
     
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
-        return 300
+        if selectedCell != nil {
+            if selectedCell.row == indexPath.row{
+                return 370
+            }
+        }
         
-        
+        return 360
     }
     
     var selectedCell: NSIndexPath! = nil
@@ -125,7 +156,7 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
         let titleLabel = UILabel(frame: CGRectMake(0, 210, (cell?.frame.width)!, 80))
         titleLabel.text = articlesToDisplay["articles"][indexPath.row]["title"].string!
         titleLabel.textColor = UIColor.whiteColor()
-        titleLabel.font = UIFont(name: "Panton-Bold", size: 40)
+        titleLabel.font = UIFont(name: "Avenir", size: 24)
         titleLabel.textAlignment = NSTextAlignment.Center
         titleLabel.numberOfLines = 2
         titleLabel.lineBreakMode = NSLineBreakMode.ByWordWrapping
@@ -137,23 +168,24 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
         summary.text = articlesToDisplay["articles"][indexPath.row]["summary"].string!
         summary.numberOfLines = 0
         summary.lineBreakMode = NSLineBreakMode.ByWordWrapping
-        if selectedCell != nil {
-            if selectedCell.row == indexPath.row{
-                    cell?.addSubview(summary)
-            }
-        }
-
+//        if selectedCell != nil {
+//            if selectedCell.row == indexPath.row{
+//                    cell?.addSubview(summary)
+//            }
+//        }
+        cell?.addSubview(summary)
 
         
-        
-        let url = articlesToDisplay["articles"][indexPath.row]["image_url"].string
-        print(url)
-        let coverImage = UIImageView(frame: CGRectMake(0, 0, cell!.frame.width, 200))
+                let coverImage = UIImageView(frame: CGRectMake(0, 0, cell!.frame.width, 200))
         coverImage.backgroundColor = UIColor.lightGrayColor()
         coverImage.contentMode = UIViewContentMode.ScaleAspectFill
         coverImage.layer.masksToBounds = true
         cell?.addSubview(coverImage)
         
+        
+        let url = articlesToDisplay["articles"][indexPath.row]["image_url"].string
+        print(url)
+
         if (url != nil){
             let image_url = NSURL(string:"https:\(url!)")
             
@@ -172,15 +204,44 @@ class SearchViewController: UIViewController, UISearchBarDelegate, UITableViewDa
             }
         }
         
+        cell?.tag = indexPath.row
+        let clickAction = UIButton(frame: CGRectMake(0, 0, cell!.frame.width, cell!.frame.height))
+        clickAction.tag = indexPath.row
+        clickAction.addTarget(self, action: "cellSelected:", forControlEvents: UIControlEvents.TouchUpInside)
+        cell?.addSubview(clickAction)
+        
         
         return cell!
     }
     
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        selectedCell = indexPath
-        tableView.reloadData()
+    func cellSelected(button: UIButton){
+//        print("cell selected \(button.tag)")
+//        selectedCell = NSIndexPath(forRow: button.tag, inSection: 0)
+//        tableView.reloadSections(NSIndexSet, withRowAnimation: <#T##UITableViewRowAnimation#>)
+        
+        let URL = NSURL(string: "https://staging.vestorly.com/api/v2/articles?vestorly_auth=eyJwYXlsb2FkIjoiNTVjNTU0YTA3ZDY4NzhhN2FjMDAwMDAxIiwiY3JlYXRlZF9vbiI6MTQzOTA0NTc5NCwic2lnbmF0dXJlIjoiZGRtaE1ZNFo4c0dFSmdHaVhvZ1lzcVc4VTdGZ1JkaHNkNiswS2ZFYThWST0ifQ&limit=300")
+        let mutableURLRequest = NSMutableURLRequest(URL: URL!)
+        mutableURLRequest.HTTPMethod  = "GET"
+        Alamofire.request(mutableURLRequest).responseJSON(completionHandler: { (req, resp, json, error) -> Void in
+            if error !=  nil {
+                print("Error")
+                print(req)
+                print(resp)
+                print(error)
+            }else{
+                print(json)
+                if json != nil {
+                    self.articlesToDisplay = JSON(json!)
+                    self.tableView.reloadSections(NSIndexSet(index: 0), withRowAnimation: UITableViewRowAnimation.Left)
+                    //                    self.loadAllImages(self.articlesToDisplay)
+                }
+            }
+        })
     }
     
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        print("cell selected")
+    }
     
     func dismissKeyboard(){
         searchBar.resignFirstResponder()
